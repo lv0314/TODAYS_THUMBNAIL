cf926861d6cc2058e918f63d5fe2271416c7b3a7
ver <luzverde0314@gmail.com>
2022-08-05 13:30:24 +0900

    feat: 파비콘 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from '../Common/Text';
 import * as S from './style';
+import FAVICON from '../../assets/icons/favicon.png';
 
 export function HeaderContainer() {
   return (
@@ -13,6 +14,9 @@ export function HeaderContainer() {
 export function Header() {
   return (
     <S.Header>
+      <S.FaviconContainer>
+        <img src={FAVICON} />
+      </S.FaviconContainer>
       <Text fontWeight="bold" text="TODAY'S THUMBNAIL" />
     </S.Header>
   );
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -12,4 +12,12 @@ export const HeaderContainer = styled.div`
 
 export const Header = styled.h3`
   margin: 16px 0 0 135px;
+  display: flex;
+  gap: 10px;
+`;
+
+export const FaviconContainer = styled.figure`
+  width: 20px;
+  height: 20px;
+  padding-bottom: 3px;
 `;
