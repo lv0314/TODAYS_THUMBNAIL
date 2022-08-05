d9df79e129ceeb15079672989294e0b7d120075f
ver <luzverde0314@gmail.com>
2022-08-05 17:36:05 +0900

    refactor: 배경화면 수정

--- src/components/Container/style.ts
+++ src/components/Container/style.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-  background: url(${BG});
+  background: url(${BG}) center center / cover no-repeat;
   display: flex;
   justify-content: center;
   align-items: center;
